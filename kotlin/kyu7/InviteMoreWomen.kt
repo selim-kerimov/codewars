82e587e3f16310dd10178563cd7df2fd6ac5a59a
selim-kerimov <selimkerimow68@gmail.com>
2025-10-01 15:20:39 +0500

    solved kata

---
+++ kotlin/kyu7/InviteMoreWomen.kt
@@ -0,0 +1,39 @@
+package kyu7
+
+// https://www.codewars.com/kata/58acfe4ae0201e1708000075/train/kotlin
+
+/**
+ * 7 kyu - Simple Fun #152: Invite More Women?
+ *
+ * ### Task
+ *
+ * King Arthur and his knights are having a New Years party. Last year Lancelot was jealous of Arthur, because Arthur
+ * had a date and Lancelot did not, and they started a duel.
+ *
+ * To prevent this from happening again, Arthur wants to make sure that there are at least as many women as men
+ * at this year's party. He gave you a list of integers of all the party goers.
+ *
+ * Arthur needs you to return true if he needs to invite more women or false if he is all set.
+ *
+ * #### Input/Output
+ * input - integer array L ($a in PHP)
+ *
+ * An array (guaranteed non-associative in PHP) representing the genders of the attendees, where -1 represents
+ * women and 1 represents men.
+ *
+ * 2 <= L.length <= 50
+ *
+ * output - a boolean value
+ *
+ * true if Arthur need to invite more women, false otherwise.
+ */
+fun inviteMoreWomen(l: List<Int>): Boolean {
+    return  l.sum() > 0
+}
+
+fun main() {
+    println(inviteMoreWomen(listOf(1, -1, 1))) // true
+    println(inviteMoreWomen(listOf(-1, -1, -1))) // false
+    println(inviteMoreWomen(listOf(1, -1))) // false
+    println(inviteMoreWomen(listOf(1, 1, 1))) // true
+}
