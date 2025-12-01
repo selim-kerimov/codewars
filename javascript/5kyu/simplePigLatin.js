7161b146352c279611ccabc57986e7042f5e2330
selim-kerimov <selimkerimow68@gmail.com>
2025-12-01 21:16:09 +0500

    solved kata

---
+++ javascript/5kyu/simplePigLatin.js
@@ -0,0 +1,25 @@
+// https://www.codewars.com/kata/520b9d2ad5c005041100000f/train/javascript
+
+/**
+ * 5 kyu - Simple Pig Latin
+ * 
+ * Move the first letter of each word to the end of it, then add "ay" to the end of the word. 
+ * Leave punctuation marks untouched.
+ * 
+ * ### Examples
+ * - pigIt('Pig latin is cool'); // igPay atinlay siay oolcay
+ * - pigIt('Hello world !');     // elloHay orldway !
+ */
+
+function pigIt(str) {
+  return str
+    .split(' ')
+    .map(word => {
+      if (!word.match(/[a,u,e,o,i,y]/gi)) return word;
+      return word.slice(1) + word[0] + 'ay'
+    })
+    .join(' ')
+}
+
+console.log(pigIt('Pig latin is cool')) // igPay atinlay siay oolcay
+console.log(pigIt('Hello world !'))     // elloHay orldway !
