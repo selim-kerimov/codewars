b9e1fa8841e353849187b01743f1332dd2325798
selim-kerimov <selimkerimow68@gmail.com>
2026-02-05 20:44:45 +0500

    solved kata

---
+++ swift/Sources/swift/8kyu/multiply.swift
@@ -0,0 +1,11 @@
+// https://www.codewars.com/kata/50654ddff44f800200000004/train/swift
+
+/**
+ * 8 kyu - Multiply
+ * 
+ * This code does not execute properly. Try to figure out why.
+ */
+
+func multiply(_ a: Double, _ b: Double) -> Double {
+  return a * b
+}
