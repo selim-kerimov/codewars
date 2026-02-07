276edc13d935d17e363c40ee3a5bb8f2747a3a38
selim-kerimov <selimkerimow68@gmail.com>
2026-02-07 15:21:04 +0500

    solved kata

---
+++ swift/Sources/swift/8kyu/opositeNumber.swift
@@ -0,0 +1,15 @@
+// https://www.codewars.com/kata/56dec885c54a926dcd001095/train/swift
+
+/**
+8 kyu - Opposite number
+
+Very simple, given a number, find its opposite (additive inverse).
+
+### Examples
+- 1: -1
+- 14: -14
+- -34: 34
+*/
+func oppositeNumber(number: Double) -> Double {
+  return number * -1
+}
