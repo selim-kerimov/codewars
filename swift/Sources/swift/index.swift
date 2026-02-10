2de2598d322584d1db4ec7c20ca30ae2b3d37815
selim-kerimov <selimkerimow68@gmail.com>
2026-02-10 10:36:24 +0500

    solved kata

--- swift/Sources/swift/index.swift
+++ swift/Sources/swift/index.swift
@@ -4,7 +4,9 @@
 @main
 struct swift {
     static func main() {
-        print(numberToReversedArray(35231))
-        print(centuryFromYear(0))
+        print(isNDivisibleByXAndY(3, 1, 3))
+        print(isNDivisibleByXAndY(12, 2, 6))
+        print(isNDivisibleByXAndY(100, 5, 3))
+        print(isNDivisibleByXAndY(12, 7, 5))
     }
 }
