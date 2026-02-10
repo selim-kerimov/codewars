1d65220c7d8f4e3ff4f8fc80f54b785da5688119
selim-kerimov <selimkerimow68@gmail.com>
2026-02-10 10:10:47 +0500

    solved kata

---
+++ swift/Sources/swift/8kyu/greeet.swift
@@ -0,0 +1,13 @@
+// https://www.codewars.com/kata/55a70521798b14d4750000a4/train/swift
+
+/**
+8 kyu - Returning Strings
+
+Create a function that accepts a parameter representing a name and returns the message: 
+"Hello, <name> how are you doing today?".
+
+[Make sure you type the exact thing I wrote or the program may not execute properly]
+*/
+func greet(_ name: String) -> String {
+  return "Hello, \(name) how are you doing today?"
+}
