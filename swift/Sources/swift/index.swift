5b473855b57467011bab83012702d8c55f8ca3e0
selim-kerimov <selimkerimovdv@gmail.com>
2026-03-23 17:02:50 +0500

    solved kata

--- swift/Sources/swift/index.swift
+++ swift/Sources/swift/index.swift
@@ -4,8 +4,7 @@
 @main
 struct swift {
     static func main() {
-        print(findOutlier([2, 4, 0, 100, 4, 11, 2602, 36]))
-        print(findOutlier([160, 3, 1719, 19, 11, 13, -21]))
-        print(findOutlier([-1942, -1930, 3168, -3606, 3456, 12, 3042, 1230, 2374, -578, -80, -1216, 3538, 198, -676, 3316, -630, 1414, -1474, 1092, 3980, 4956, -3126, 1862, 4170, 1996, 2282, -2693, -4074, 2084]))
+        print(countDuplicates("indivisibility"))
+        print(countDuplicates("aA11"))
     }
 }
